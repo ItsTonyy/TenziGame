077909c2d2d2338f3f5b90c574114b33f98e971d
Tony <tonytnportela@outlook.com>
2023-07-04 00:25:34 -0300

    new things idk

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,29 @@
+import { useState } from 'react'
 import Dice from './components/Dice'
 
 function App() {
   // Generating an array of 10 numbers and the maximum number generated being 6, plus no 0 in array.
 	const allNewDice = () => Array.from({length: 10}, () => Math.floor(Math.random() * 6) + 1)
-  const arr = allNewDice()
+
+  const [dice, setDice] = useState({
+    value: allNewDice(),
+    isHeld: false
+  })
+
+  const diceElements = dice.value.map(number => <Dice value={number} />)
+
+  const rollDice = () => {
+    setDice({...dice, value: allNewDice()})
+  }
 
 	return (
 		<>
 			<main>
         <div className="outer-container">
           <div className='container'>
-            <Dice value={arr[0]} />
-            <Dice value={arr[1]} />
-            <Dice value={arr[2]} />
-            <Dice value={arr[3]} />
-            <Dice value={arr[4]} />
-            <Dice value={arr[5]} />
-            <Dice value={arr[6]} />
-            <Dice value={arr[7]} />
-            <Dice value={arr[8]} />
-            <Dice value={arr[9]} />
+            {diceElements}
           </div>
+          <button className='roll-button' onClick={rollDice}>Roll</button>
         </div>
 			</main>
 		</>
