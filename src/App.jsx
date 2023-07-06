af2841aedf8fe52cb0eae6520998694197f44db5
Tony <tonytnportela@outlook.com>
2023-07-05 23:30:30 -0300

    new stuff

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,37 @@ import Dice from './components/Dice'
 function App() {
 	const [dice, setDice] = useState(allNewDice())
 
+	function DiceDefault() {
+		return {
+			value: Math.floor(Math.random() * 6) + 1,
+			isHeld: false,
+			id: nanoid(),
+		}
+	}
+
 	// Generating an array of 10 numbers and the maximum number generated being 6, plus no 0 in array.
 	function allNewDice() {
 		const newDice = []
 		for (let i = 0; i < 10; i++) {
-			newDice.push({
-				value: Math.floor(Math.random() * 6) + 1,
-				isHeld: false,
-				id: nanoid(),
-			})
+			newDice.push(DiceDefault())
 		}
 		return newDice
 	}
 
 	const rollDice = () => {
-		setDice(allNewDice())
+		setDice((oldDice) =>
+			oldDice.map((die) => {
+				return die.isHeld ? { ...die } : DiceDefault()
+			})
+		)
 	}
 
 	const holdDice = (id) => {
-		console.log(id)
+		setDice((oldDice) =>
+			oldDice.map((die) => {
+				return die.id === id ? { ...die, isHeld: !die.isHeld } : { ...die }
+			})
+		)
 	}
 
 	const diceElements = dice.map((newDiceTwin) => (
@@ -38,12 +50,25 @@ function App() {
 	return (
 		<>
 			<main>
+
+				<div className='intro'>
+					<h1 className='title'>Tenzies</h1>
+					<p className='p1'>
+						Roll until all dice are the same.
+					</p>
+
+					<p className='p2'>
+					Click each die to freeze it at its current value between rolls.
+					</p>
+				</div>
+
 				<div className='outer-container'>
 					<div className='container'>{diceElements}</div>
 					<button className='roll-button' onClick={rollDice}>
 						Roll
 					</button>
 				</div>
+
 			</main>
 		</>
 	)
